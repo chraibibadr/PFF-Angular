91a78091c5e439ad4d04b652f21b95ec4a9ce05b
mohamed maine <mohamed.mauin@gmail.com>
2020-03-20 15:29:32 +0100

    simo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { FormateurComponent } from './Components/formateur/formateur.component';
 import { TaskComponent } from './Components/task/task.component';
 import { LoginComponent } from './Components/login/login.component';
 import { DeposerComponent } from './Components/deposer/deposer.component';
-
+import { AdminHomeComponent } from './Components/admin-home/admin-home.component';
 
 const routes: Routes = [
   {path:"",redirectTo:'home',pathMatch:'full'},
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path:'task',component:TaskComponent},
   {path:'login',component:LoginComponent},
   {path:'deposer',component:DeposerComponent},
-  
+  {path:'admin-home',component:AdminHomeComponent},
 ];
 
 @NgModule({
